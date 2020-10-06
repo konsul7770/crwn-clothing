463a899d1af1244096668fd70f9fd033de15e766
konsul777 <konsul7770@gmail.com>
2020-10-06 12:31:03 +0300

    updated our selector itemCount in cart-icon component with library reselect and memorized selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,18 +4,24 @@ import "./cart-icon.styles.scss";
 
 import {connect} from "react-redux";
 import {toggleCartsHidden} from "../../redux/cart/cart.actions";
+import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
 
-const CartIcon = ({toggleCartsHidden}) => (
+const CartIcon = ({toggleCartsHidden,itemCount}) => (
     <div className="cart-icon"
          onClick={toggleCartsHidden}
     >
         <ShoppingIcon className = "shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
+
 
 const mapDispatchToProps = dispatch =>({
     toggleCartsHidden: () => dispatch(toggleCartsHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
