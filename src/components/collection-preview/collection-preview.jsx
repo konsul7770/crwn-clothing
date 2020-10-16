fb4786eed772e893e1006540f54edc3dfc09d8d9
konsul777 <konsul7770@gmail.com>
2020-10-16 21:33:55 +0300

    implementing code that pulls in our shop data from firestore and stores it into our shop reducer from our shop page

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./collection-preview.styles.scss";
 
+import {withRouter} from "react-router-dom";
+
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({title, items,match,history}) => (
     <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
+        <h1
+            className="title"
+            onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}
+        >{title.toUpperCase()}</h1>
         <div className="preview">
             {
                 items.filter((item,idx) => idx<4)
@@ -16,4 +21,4 @@ const CollectionPreview = ({title, items}) => (
         </div>
     </div>
 );
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
